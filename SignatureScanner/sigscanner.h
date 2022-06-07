6a7adfc091cd7f451cf84b9a01316cb242e0c1ea
Reza Feizi <RezaFeizi@iran.ir>
2022-06-07 06:40:11 +0430

    Update sigscanner.h
    * Support x64
    * Add GetProcessID
    * Fixed #1

--- SignatureScanner/sigscanner.h
+++ SignatureScanner/sigscanner.h
@@ -2,7 +2,6 @@
 
 #include <iostream>
 #include <string>
-
 #include <Windows.h>
 #include <TlHelp32.h>
 
@@ -13,96 +12,138 @@ using std::cout;
 using std::endl;
 using std::string;
 
-// datatype for a module in memory (dll, regular exe) 
+// datatype for a module in memory (dll, regular exe)
 struct module
 {
-	DWORD dwBase, dwSize;
+    DWORD64 dwBase, dwSize;
 };
 
 class SignatureScanner
 {
 public:
     module TargetModule;  // Hold target module
-	HANDLE TargetProcess; // for target process
-	DWORD  TargetId;      // for target process
+    HANDLE TargetProcess; // for target process
+    DWORD  TargetId;      // for target process
 
-	// For getting a handle to a process
-	HANDLE GetProcess(char* processName)
-	{
-		HANDLE handle = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
-		PROCESSENTRY32 entry;
-		entry.dwSize = sizeof(entry);
 
-		do
-			if (!strcmp(entry.szExeFile, processName)) {
-				TargetId = entry.th32ProcessID;
-				CloseHandle(handle);
-				TargetProcess = OpenProcess(PROCESS_ALL_ACCESS, false, TargetId);
-				return TargetProcess;
-			}
-		while (Process32Next(handle, &entry));
+    // For getting a handle to a process
+    HANDLE GetProcess(const char* processName)
+    {
+        HANDLE handle = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
+        PROCESSENTRY32 entry;
+        entry.dwSize = sizeof(entry);
+        do
+            if(!strcmp(entry.szExeFile, processName))
+            {
+                TargetId = entry.th32ProcessID;
+                CloseHandle(handle);
+                TargetProcess = OpenProcess(PROCESS_ALL_ACCESS, false, TargetId);
+                return TargetProcess;
+            }
+        while(Process32Next(handle, &entry));
 
-		return false;
-	}
+        return false;
+    }
 
-	// For getting information about the executing module
-	module GetModule(char* moduleName) {
-		HANDLE hmodule = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, TargetId);
-		MODULEENTRY32 mEntry;
-		mEntry.dwSize = sizeof(mEntry);
+    // For getting information about the executing module
+    module GetModule(const char* moduleName)
+    {
+        HANDLE hmodule = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, TargetId);
+        MODULEENTRY32 mEntry;
+        mEntry.dwSize = sizeof(mEntry);
 
-		do {
-			if (!strcmp(mEntry.szModule, (LPSTR)moduleName)) {
-				CloseHandle(hmodule);
+        do
+        {
+            if(!strcmp(mEntry.szModule, (LPSTR)moduleName))
+            {
+                CloseHandle(hmodule);
 
-				TargetModule = { (DWORD)mEntry.hModule, mEntry.modBaseSize };
-				return TargetModule;
-			}
-		} while (Module32Next(hmodule, &mEntry));
+                TargetModule = { (DWORD64)mEntry.hModule, (DWORD64)mEntry.modBaseSize };
+                return TargetModule;
+            }
+        }
+        while(Module32Next(hmodule, &mEntry));
 
-		module mod = { (DWORD)false, (DWORD)false };
-		return mod;
-	}
+        module mod = { (DWORD_PTR)false, (DWORD_PTR)false };
+        return mod;
+    }
 
-	// Basic WPM wrapper, easier to use.
-	template <typename var>
-	bool WriteMemory(DWORD Address, var Value) {
-		return WriteProcessMemory(TargetProcess, (LPVOID)Address, &Value, sizeof(var), 0);
-	}
+    DWORD GetProcessID(const char* ProcessName) // now returns the process ID. Why mess around with globals?
+    {
+        PROCESSENTRY32   pe32;
+        HANDLE         hSnapshot = NULL;
+        DWORD pid = 0; // initialize to impossible pid
 
-	// Basic RPM wrapper, easier to use.
-	template <typename var>
-	var ReadMemory(DWORD Address) {
-		var value;
-		ReadProcessMemory(TargetProcess, (LPCVOID)Address, &value, sizeof(var), NULL);
-		return value;
-	}
+        pe32.dwSize = sizeof(PROCESSENTRY32);
+        hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
 
-	// for comparing a region in memory, needed in finding a signature
-	bool MemoryCompare(const BYTE* bData, const BYTE* bMask, const char* szMask) {
-		for (; *szMask; ++szMask, ++bData, ++bMask) {
-			if (*szMask == 'x' && *bData != *bMask) {
-				return false;
-			}
-		}
-		return (*szMask == NULL);
-	}
+        // should probably check for hSnapshot != INVALID_HANDLE_VALUE right about here
+        if(Process32First(hSnapshot, &pe32))
+        {
+            do
+            {
+                if(strcmp(pe32.szExeFile, ProcessName) == 0)
+                {
+                    pid = pe32.th32ProcessID;
+                    break;
+                }
+            }
+            while(Process32Next(hSnapshot, &pe32));
+        }
 
-	// for finding a signature/pattern in memory of another process
-	DWORD FindSignature(DWORD start, DWORD size, const char* sig, const char* mask)
-	{
-		BYTE* data = new BYTE[size];
-		SIZE_T bytesRead;
+        if(hSnapshot != INVALID_HANDLE_VALUE)
+            CloseHandle(hSnapshot);
 
-		ReadProcessMemory(TargetProcess, (LPVOID)start, data, size, &bytesRead);
+        return pid;
+    }
 
-		for (DWORD i = 0; i < size; i++)
-		{
-			if (MemoryCompare((const BYTE*)(data + i), (const BYTE*)sig, mask)) {
-				return start + i;
-			}
-		}
-		delete[] data;
-		return NULL;
-	}
+    // Basic WPM wrapper, easier to use.
+    template <typename var>
+    bool WriteMemory(DWORD64 Address, var Value)
+    {
+        return WriteProcessMemory(TargetProcess, (LPVOID)Address, &Value, sizeof(var), 0);
+    }
+
+    // Basic RPM wrapper, easier to use.
+    template <typename var>
+    var ReadMemory(DWORD64 Address)
+    {
+        var value;
+        ReadProcessMemory(TargetProcess, (LPCVOID)Address, &value, sizeof(var), NULL);
+        return value;
+    }
+
+    // for comparing a region in memory, needed in finding a signature
+    bool MemoryCompare(const BYTE* bData, const BYTE* bMask, const char* szMask)
+    {
+        for(; *szMask; ++szMask, ++bData, ++bMask)
+        {
+            if(*szMask == 'x' && *bData != *bMask)
+            {
+                return false;
+            }
+        }
+        return (*szMask == NULL);
+    }
+
+    // for finding a signature/pattern in memory of another process
+    DWORD64 FindSignature(DWORD64 start, DWORD64 size, const char* sig, const char* mask)
+    {
+        BYTE* data = new BYTE[size];
+        SIZE_T bytesRead;
+        DWORD64 result = NULL;
+
+        ReadProcessMemory(TargetProcess, (LPVOID)start, data, size, &bytesRead);
+
+        for(DWORD64 i = 0; i < size; i++)
+        {
+            if(MemoryCompare((const BYTE*)(data + i), (const BYTE*)sig, mask))
+            {
+                result = start + i;
+                break;
+            }
+        }
+        delete[] data;
+        return result;
+    }
 };
